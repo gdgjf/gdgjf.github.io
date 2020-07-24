e557f010844f4ea34c6816095da9e8aac5eb58a1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-24 12:18:24 +0000

    Deploy gdgjf/gdgjf.github.io to github.com/gdgjf/gdgjf.github.io.git:next_app_test_deploy

---
+++ _next/static/FV_t2QrnsK1_zb_bJHubj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/about":[a],"/users":[a],"/users/[id]":[a]}}("static\u002Fchunks\u002F3604e557afa629591620afe8e318261bc9fa9b4a.d5233a8295dad699ee4f.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/FV_t2QrnsK1_zb_bJHubj/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fusers","\u002Fusers\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
