823de5d98227b18fa2b94df3d723bd13a8947a7c
lesleyandrez <lesley.andrez@gmail.com>
2020-09-11 19:54:17 -0300

    Ajustes visuais  - Ajusta cores de fundo das seções para ficar intercalado  - Adicionada logo em SVG, j's com uma preparação para uma logo responsiva WIP

--- partials/header.tsx
+++ partials/header.tsx
@@ -1,17 +1,31 @@
-import Menu from "./menu"
+import Menu from "./menu";
 
 const Header = () => (
-    <header className="main-header">
-        <div className="main-wrapper">
-            <div className="logo-wrapper">
-                <a href="/">
-                    <img className="logo-image" src="images/gdgjf-chapter-logo.png"/>
-                </a>
-            </div>
-            <div className="flex"></div>
-            <Menu></Menu>
-        </div>
-    </header>
-)
+  <header className="main-header">
+    <div className="main-wrapper">
+      <div className="logo-wrapper">
+        <a href="/">
+          {/* <img className="logo-image" src="images/gdgjf-chapter-logo.png"/> */}
+          <img style={{ height: "30px" }} src="images/gdg-logo.svg" alt="" />
+          <div>
+            <img
+                style={{ height: "20px", transform: "translateX(10px)" }}
+                src="images/j.svg"
+                alt=""
+            />
+            <img
+                style={{ height: "20px", transform: "translateX(88px)" }}
+                src="images/f.svg"
+                alt=""
+            />
+            <img style={{ height: "20px" }} src="images/uiz-de-ora.svg" alt="" />
+          </div>
+        </a>
+      </div>
+      <div className="flex"></div>
+      <Menu></Menu>
+    </div>
+  </header>
+);
 
-export default Header
+export default Header;
