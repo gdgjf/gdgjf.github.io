ccb2c89334417a71fdb56205c792ba134cc901e8
Vinicius Soranço <vinicius.soranco@levty.com>
2020-10-05 09:38:25 -0300

    fix: correção erro CI e ajuste estilo botão

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -21,59 +21,59 @@ function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
   })
 }
 
-// Fetching all GDG organization members on Github
-async function fetchMembers(token: string | null){
-  return fetch('https://api.github.com/orgs/gdgjf/members', {
-    headers: {
-      Authorization: `token ${token}`
-    }
-  })
-  .then(resp => resp.json());
-}
+// // Fetching all GDG organization members on Github
+// async function fetchMembers(token: string | null){
+//   return fetch('https://api.github.com/orgs/gdgjf/members', {
+//     headers: {
+//       Authorization: `token ${token}`
+//     }
+//   })
+//   .then(resp => resp.json());
+// }
 
-// Fetching all GDG Contributors on Github
-async function fetchContributors(token: string | null){
-  let allContributors: any[] = [];
-  const repos = await fetch('https://api.github.com/orgs/gdgjf/repos', {
-    headers: {
-      Authorization: `token ${token}`,
-      Accept: "application/vnd.github.v3+json"
-    }
-  })
-  .then(resp => resp.json());
-  if(repos){
-    for(const repo of repos){
-      await fetch(`https://api.github.com/repos/gdgjf/${repo.name}/contributors`,{
-        headers: {
-          Authorization: `token ${token}`,
-          Accept: "application/vnd.github.v3+json"
-        }
-      })
-        .then(resp => resp.json())
-        .then(contributorsPerRepo => {
-          contributorsPerRepo.forEach((contributor:any) => {
-            if(!allContributors.some(obj => obj.login === contributor.login)){
-              allContributors.push(contributor);
-            }
-          })
-        })
-    }
-    return allContributors;
-  }
-}
+// // Fetching all GDG Contributors on Github
+// async function fetchContributors(token: string | null){
+//   let allContributors: any[] = [];
+//   const repos = await fetch('https://api.github.com/orgs/gdgjf/repos', {
+//     headers: {
+//       Authorization: `token ${token}`,
+//       Accept: "application/vnd.github.v3+json"
+//     }
+//   })
+//   .then(resp => resp.json());
+//   if(repos){
+//     for(const repo of repos){
+//       await fetch(`https://api.github.com/repos/gdgjf/${repo.name}/contributors`,{
+//         headers: {
+//           Authorization: `token ${token}`,
+//           Accept: "application/vnd.github.v3+json"
+//         }
+//       })
+//         .then(resp => resp.json())
+//         .then(contributorsPerRepo => {
+//           contributorsPerRepo.forEach((contributor:any) => {
+//             if(!allContributors.some(obj => obj.login === contributor.login)){
+//               allContributors.push(contributor);
+//             }
+//           })
+//         })
+//     }
+//     return allContributors;
+//   }
+// }
 
-// Getting the role based on which list user is inside
-function getUserRole(user: any, organizationMembers: any[], contributors: any[]) {
-  if(organizationMembers.some((obj: any) => obj.login === user.login)){
-    return ["Embaixador", "Organizador", "Mentor", "Apoiador", "Participante"];
-  }
-  else if(contributors.some((obj: any) => obj.login === user.login)){
-    return ["Mentor", "Apoiador"];
-  }
-  else{
-    return ["Participante"];
-  }
-}
+// // Getting the role based on which list user is inside
+// function getUserRole(user: any, organizationMembers: any[], contributors: any[]) {
+//   if(organizationMembers.some((obj: any) => obj.login === user.login)){
+//     return ["Embaixador", "Organizador", "Mentor", "Apoiador", "Participante"];
+//   }
+//   else if(contributors.some((obj: any) => obj.login === user.login)){
+//     return ["Mentor", "Apoiador"];
+//   }
+//   else{
+//     return ["Participante"];
+//   }
+// }
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if(!req.body) return;
@@ -102,7 +102,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         }
       })
       .then(resp => resp.json())
-      .then(async user => {
+      .then(user => {
         
         //* Dinamic fetching almost complete, just missing collaborators
         // const organizationMembers: Array<any> = await fetchMembers(access_token!);
@@ -127,18 +127,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
           roles = ["Participante"];
         }
 
-        return res.status(200).send({
-          avatar_url: user.avatar_url,
-          bio: user.bio,
-          blog: user.blog,
-          html_url: user.html_url,
-          id: user.id,
-          login: user.login,
-          name: user.name,
-          node_id: user.node_id,
-          url: user.url,
-          roles
-        })})
+        return res
+          .status(200)
+          .send({
+            avatar_url: user.avatar_url,
+            bio: user.bio,
+            blog: user.blog,
+            html_url: user.html_url,
+            id: user.id,
+            login: user.login,
+            name: user.name,
+            node_id: user.node_id,
+            url: user.url,
+            roles
+          });
+      })
       .catch((error) => res.status(400).json(error));
     } catch (e) {
       return res.status(400).json(e);
