ad96eab3ab7050d4abe2fc1328b8cd1e89d394ab
Lucas Coelho <lucascoelhodacosta@gmail.com>
2025-04-29 16:15:04 -0300

    chore: add missing env

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
