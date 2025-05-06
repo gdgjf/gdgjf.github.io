1a1078af390de505c568ffaf0887c864f69d7093
Lucas Coelho <lucascoelhodacosta@gmail.com>
2025-05-06 15:44:39 -0300

    chore: update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         {children}
